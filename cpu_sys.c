78045772ba3c27caf70c5d8dd2a2e78f8b6f0363
Jens Elkner <jel+solmex-src@cs.ovgu.de>
2025-04-24 20:57:49 +0200

    cpu_sys.c, vmstat.c: make description usage uniform
    - use snames the same way as in network.c (and fix c+p bugs)
    - rename *_ticks_bla to *_bla_ticks
    - add examples to man page

--- cpu_sys.c
+++ cpu_sys.c
@@ -33,7 +33,7 @@ static ks_info_t kstat[KS_IDX_MAX] = {
 #pragma GCC diagnostic pop
 
 /** Ja, auch hier wieder etwas schreibfaul, also auch generisch ;-) */
-static const char *snames[] = {
+static const char *knames[] = {
 	"cpu_ticks_idle",	"cpu_ticks_kernel",	"cpu_ticks_user",
 	"cpu_ticks_wait",	"intr",				"inv_swtch",
 	"namei",			"nthreads",			"phread",
@@ -56,6 +56,23 @@ typedef enum sys_idx {
 	SYS_IDX_MUTEXIN,		SYS_IDX_RDFAILS,			SYS_IDX_WRFAILS,
 	SYS_IDX_MAX
 } sys_idx_t;
+
+/* solmex metric names */
+static const char *snames[] = {
+#define STRINGIFY(x) #x
+#define _S(x) STRINGIFY(x)
+	_S(SOLMEX_SYS_CPU_TICKS_IDLE_N),	_S(SOLMEX_SYS_CPU_TICKS_KERNEL_N),	_S(SOLMEX_SYS_CPU_TICKS_USER_N),
+	_S(SOLMEX_SYS_CPU_TICKS_WAIT_N),	_S(SOLMEX_SYS_INTR_N),				_S(SOLMEX_SYS_INV_SWTCH_N),
+	_S(SOLMEX_SYS_NAMEI_N),				_S(SOLMEX_SYS_NTHREADS_N),			_S(SOLMEX_SYS_PHREAD_N),
+	_S(SOLMEX_SYS_PHWRITE_N),			_S(SOLMEX_SYS_PSWITCH_N),			_S(SOLMEX_SYS_SYSCALL_N),
+	_S(SOLMEX_SYS_SYSEXEC_N),			_S(SOLMEX_SYS_SYSFORK_N),			_S(SOLMEX_SYS_SYSREAD_N),
+	_S(SOLMEX_SYS_SYSVFORK_N),			_S(SOLMEX_SYS_SYSWRITE_N),			_S(SOLMEX_SYS_TRAP_N),
+	_S(SOLMEX_SYS_XCALLS_N),
+	// to be able to simulate mpstat:
+	_S(SOLMEX_SYS_IRQTHREAD_N),			_S(SOLMEX_SYS_MIGRATE_N),
+	_S(SOLMEX_SYS_MUTEXIN_N),			_S(SOLMEX_SYS_RDFAILS_N),			_S(SOLMEX_SYS_WRFAILS_N),
+	NULL
+};
 
 static const char *sdesc[] = {
 	SOLMEX_SYS_CPU_TICKS_IDLE_D,	SOLMEX_SYS_CPU_TICKS_KERNEL_D,	SOLMEX_SYS_CPU_TICKS_USER_D,
@@ -161,7 +178,7 @@ getx:
 #pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
 		for (l = 0; l < what_sz; l++) {
 			k = what[l];
-			if ((knp = kstat_data_lookup(ksp, snames[k])) != NULL) {
+			if ((knp = kstat_data_lookup(ksp, knames[k])) != NULL) {
 				vals[sidx + k] += knp->value.ui64;
 				if (mp)
 					vals[idx + k] = knp->value.ui64;
@@ -184,12 +201,11 @@ valx:
 	for (l = 0; l < what_sz; l++) {
 		k = what[l];
 		if (!compact)
-			addPromInfo4(SOLMEX_SYS_NAME_PREFIX, snames[k], "counter", sdesc[k]);
+			addPromInfo4("", snames[k], "counter", sdesc[k]);
 		idx = 0;
 		for (i = mp ? 0 : n; i <= n; i++, idx += SYS_IDX_MAX) {
 			if (!seen[i])
 				continue;
-			psb_add_str(sb, SOLMEX_SYS_NAME_PREFIX);
 			psb_add_str(sb, snames[k]);
 			if (i == n) {
 				sprintf(buf, "{cpu=\"sum\"} %ld\n", vals[sidx + k]);
